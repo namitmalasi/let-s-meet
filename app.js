fb22b5ffab8928b9dbe7b2545bfe180d220ff03f
Namit Malasi <namitmalasi@gmail.com>
2021-10-13 14:00:38 +0530

    connected srever and client with scoket.io

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const http = require("http");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected on Socket.io server");
+  console.log(socket.id);
 });
 
 server.listen(PORT, () => {
