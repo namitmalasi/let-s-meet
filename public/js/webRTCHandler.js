b683af5a3b42b4a60b279d4e949edfc80ee3c797
Namit Malasi <namitmalasi@gmail.com>
2021-10-16 11:38:27 +0530

    getting access to camera and mic'

--- public/js/webRTCHandler.js
+++ public/js/webRTCHandler.js
@@ -1,8 +1,27 @@
 import * as wss from "./wss.js";
 import * as constants from "./constants.js";
 import * as ui from "./ui.js";
+import * as store from "./store.js";
 
 let connectedUserDetails;
+
+const defaultConstraints = {
+  audio: true,
+  video: true,
+};
+
+export const getLocalPreview = () => {
+  navigator.mediaDevices
+    .getUserMedia(defaultConstraints)
+    .then((stream) => {
+      ui.updateLocalVideo(stream);
+      store.setLocalStream(stream);
+    })
+    .catch((err) => {
+      console.log("error occured when trying to get access to camera");
+      console.log(err);
+    });
+};
 export const sendPreOffer = (callType, calleePersonalCode) => {
   connectedUserDetails = {
     callType,
